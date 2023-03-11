aca5595e559dc56ab5f14c5f23065a504fb1afd8
AlexVedernikoff <templar_sc@mail.ru>
2023-03-12 01:51:10 +0300

    добавили навигационную панель NavNar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { AppBar, Toolbar, Grid, Button } from "@material-ui/core";
 
 const NavBar = () => {
     return (
-        <div>
-            NavBar
-        </div>
+        <AppBar position="static" style={{ backgroundColor: "#00897b" }}>
+            <Toolbar>
+                <Grid container justify={"flex-end"}>
+                    <Button variant={"outlined"}>Логин</Button>
+                    <Button variant={"outlined"}>Выйти</Button>
+                </Grid>
+            </Toolbar>
+        </AppBar >
     )
 }
 
